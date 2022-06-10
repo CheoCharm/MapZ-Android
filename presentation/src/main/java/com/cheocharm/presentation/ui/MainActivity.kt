b9b128d2dec3bf60adf5028bbe8e3c008682a9a3
신민정 <ydaispaxero@naver.com>
2022-06-10 10:30:30 +0900

    #1 feat: presentation 레이어에 MainActivity 추가

---
+++ presentation/src/main/java/com/cheocharm/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cheocharm.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cheocharm.presentation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
