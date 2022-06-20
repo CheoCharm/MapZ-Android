3edaae390b6f5566a54242bf03c3b7d2853c330d
Hyesung82 <khs990419@gmail.com>
2022-06-20 23:26:25 +0900

    #4 feat: 메인 화면 bottom nav 구현

--- presentation/src/main/java/com/cheocharm/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/cheocharm/presentation/ui/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.cheocharm.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.cheocharm.presentation.R
+import com.cheocharm.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+
+        setContentView(binding.root)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = binding.bottomNavMain
+        bottomNav.setupWithNavController(navController)
     }
 }
