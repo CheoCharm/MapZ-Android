53258f12b941b8d8ac87b33f420046833c2dabc0
신민정 <ydaispaxero@naver.com>
2022-06-21 11:45:10 +0900

    #12 feat: base 모듈에 BaseActivity, BaseFragment 추가

---
+++ base/src/main/java/com/cheocharm/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.cheocharm.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes val layoutResId: Int) :
+    AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+        binding.lifecycleOwner = this
+    }
+}
