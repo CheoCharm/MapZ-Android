fcdd1e6555e2a3c354a3b8076a4089b72ea7e706
신민정 <ydaispaxero@naver.com>
2022-07-05 16:07:12 +0900

    #19 feat: Interceptor 추가

---
+++ remote/src/main/java/com/cheocharm/remote/di/RemoteModule.kt
@@ -0,0 +1,48 @@
+package com.cheocharm.remote.di
+
+import com.cheocharm.remote.network.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AuthOkHttpClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class WithoutAuthOkHttpClient
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object RemoteModule {
+
+    @AuthOkHttpClient
+    @Provides
+    fun provideAuthOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @WithoutAuthOkHttpClient
+    @Provides
+    fun provideWithoutAuthOkHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+}
