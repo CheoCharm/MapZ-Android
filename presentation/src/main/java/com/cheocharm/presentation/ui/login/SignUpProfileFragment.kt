81b927891f24247601f712168261874d6ecd5a48
신민정 <ydaispaxero@naver.com>
2022-07-10 17:49:40 +0900

    #23 feat: 회원가입의 프로필 설정 화면 작성

---
+++ presentation/src/main/java/com/cheocharm/presentation/ui/login/SignUpProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.cheocharm.presentation.ui.login
+
+import com.cheocharm.base.BaseFragment
+import com.cheocharm.presentation.R
+import com.cheocharm.presentation.databinding.FragmentSignUpProfileBinding
+
+class SignUpProfileFragment :
+    BaseFragment<FragmentSignUpProfileBinding>(R.layout.fragment_sign_up_profile) {
+}
