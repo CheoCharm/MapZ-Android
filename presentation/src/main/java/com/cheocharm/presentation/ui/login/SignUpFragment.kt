9e0825b62ee9de38d8939331312f416e7630a6a1
신민정 <ydaispaxero@naver.com>
2022-07-10 17:06:35 +0900

    #23 feat: 회원가입 화면 작성

---
+++ presentation/src/main/java/com/cheocharm/presentation/ui/login/SignUpFragment.kt
@@ -0,0 +1,9 @@
+package com.cheocharm.presentation.ui.login
+
+import com.cheocharm.base.BaseFragment
+import com.cheocharm.presentation.R
+import com.cheocharm.presentation.databinding.FragmentSignUpBinding
+
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
+
+}
