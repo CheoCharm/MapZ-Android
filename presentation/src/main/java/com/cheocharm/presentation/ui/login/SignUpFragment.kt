0a0b5b4c1251e165e11afa821841bb3437546325
신민정 <ydaispaxero@naver.com>
2022-07-11 09:56:15 +0900

    #23 feat: 회원가입 화면의 back 버튼 수정 및 화면의 paddingBottom 수정

--- presentation/src/main/java/com/cheocharm/presentation/ui/login/SignUpFragment.kt
+++ presentation/src/main/java/com/cheocharm/presentation/ui/login/SignUpFragment.kt
@@ -1,9 +1,22 @@
 package com.cheocharm.presentation.ui.login
 
+import android.os.Bundle
+import android.view.View
 import com.cheocharm.base.BaseFragment
 import com.cheocharm.presentation.R
 import com.cheocharm.presentation.databinding.FragmentSignUpBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initButton()
+    }
+
+    private fun initButton() {
+
+    }
 }
