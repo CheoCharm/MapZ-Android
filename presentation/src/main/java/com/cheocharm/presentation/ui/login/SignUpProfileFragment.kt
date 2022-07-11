22e14d5a5c108d64085c95a9f424a9c0236bf4df
신민정 <ydaispaxero@naver.com>
2022-07-11 09:49:35 +0900

    #23 feat: 프로필 설정 화면의 back 버튼 기능 추가 (navigation)

--- presentation/src/main/java/com/cheocharm/presentation/ui/login/SignUpProfileFragment.kt
+++ presentation/src/main/java/com/cheocharm/presentation/ui/login/SignUpProfileFragment.kt
@@ -1,9 +1,26 @@
 package com.cheocharm.presentation.ui.login
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.cheocharm.base.BaseFragment
 import com.cheocharm.presentation.R
 import com.cheocharm.presentation.databinding.FragmentSignUpProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpProfileFragment :
     BaseFragment<FragmentSignUpProfileBinding>(R.layout.fragment_sign_up_profile) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initButton()
+    }
+
+    private fun initButton() {
+        binding.btnSignUpProfileBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
