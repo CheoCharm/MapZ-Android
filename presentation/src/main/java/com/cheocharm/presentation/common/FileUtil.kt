f826f4d09f873d3f40f754a6aa20ec0528ce3584
신민정 <ydaispaxero@naver.com>
2022-08-15 22:57:16 +0900

    #23 feat: Uri -> File 변환을 위한 FileUtil, UriUtil 생성

---
+++ presentation/src/main/java/com/cheocharm/presentation/common/FileUtil.kt
@@ -0,0 +1,27 @@
+package com.cheocharm.presentation.common
+
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import java.io.File
+import java.io.FileOutputStream
+
+object FileUtil {
+    fun createTempFile(context: Context, fileName: String): File {
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File(storageDir, fileName)
+    }
+
+    fun copyToFile(context: Context, uri: Uri, file: File) {
+        val inputStream = context.contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(file)
+
+        val buffer = ByteArray(4 * 1024)
+        while (true) {
+            val byteCount = inputStream?.read(buffer) ?: return
+            if (byteCount < 0) break
+            outputStream.write(buffer, 0, byteCount)
+        }
+        outputStream.flush()
+    }
+}
