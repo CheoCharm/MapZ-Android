d8417fc721dcf6f07e540f4af446e115776a8aa8
Hyesung82 <khs990419@gmail.com>
2022-08-17 11:12:04 +0900

    feat: 선택된 위치 출력 형식 수정

--- presentation/src/main/java/com/cheocharm/presentation/model/Picture.kt
+++ presentation/src/main/java/com/cheocharm/presentation/model/Picture.kt
@@ -1,12 +1,13 @@
 package com.cheocharm.presentation.model
 
 import android.net.Uri
+import com.cheocharm.presentation.util.toCoordString
 import com.google.android.gms.maps.model.LatLng
 
 data class Picture(val uri: Uri, val latLng: LatLng?) {
     var address: String? = null
 
     fun getLocationString(): String {
-        return address ?: latLng.toString()
+        return address ?: latLng.toCoordString() ?: "현재 위치"
     }
 }
--- presentation/src/main/java/com/cheocharm/presentation/util/GeocodeUtil.kt
+++ presentation/src/main/java/com/cheocharm/presentation/util/GeocodeUtil.kt
@@ -1,7 +1,6 @@
 package com.cheocharm.presentation.util
 
 import android.content.Context
-import android.location.Address
 import android.location.Geocoder
 import com.cheocharm.presentation.model.Picture
 import java.util.*
@@ -13,17 +12,13 @@ object GeocodeUtil {
     ) {
         picture.latLng?.let {
             try {
-                val addresses: MutableList<Address>
                 val geocoder = Geocoder(context, Locale.KOREAN)
-                addresses = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                val addresses = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+
                 if (addresses.isEmpty().not()) {
                     val fetchedAddress = addresses.first()
-                    val index = fetchedAddress.maxAddressLineIndex
-
-                    if (index >= 0) {
-                        val address = fetchedAddress.getAddressLine(0)
-                        picture.address = address
-                    }
+                    val address = fetchedAddress.getAddressLine(0)
+                    picture.address = address
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
