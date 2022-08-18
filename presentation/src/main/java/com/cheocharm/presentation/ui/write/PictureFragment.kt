823f367f42e3e2cfe42257c3114e38ad7d6abde6
Hyesung82 <khs990419@gmail.com>
2022-08-19 01:01:17 +0900

    feat: external storage의 위치 정보 접근 권한 추가

--- presentation/src/main/java/com/cheocharm/presentation/ui/write/PictureFragment.kt
+++ presentation/src/main/java/com/cheocharm/presentation/ui/write/PictureFragment.kt
@@ -1,6 +1,6 @@
 package com.cheocharm.presentation.ui.write
 
-import android.net.Uri
+import android.Manifest
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
@@ -18,13 +18,21 @@ import com.google.android.gms.maps.model.LatLng
 class PictureFragment : BaseFragment<FragmentPictureBinding>(R.layout.fragment_picture) {
     private val pictureViewModel: PictureViewModel by navGraphViewModels(R.id.write)
 
+    private val requestPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.forEach {
+                if (it.value.not()) {
+                    // TODO: 권한 없을 때 처리
+                }
+            }
+        }
     private val getContent =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-            uri?.let {
+        registerForActivityResult(ActivityResultContracts.GetContent()) {
+            it?.let { uri ->
                 requireContext().contentResolver.openInputStream(uri)?.let { inputStream ->
                     val exif = ExifInterface(inputStream)
-                    val latLng = exif.latLong?.let {
-                        LatLng(it[0], it[1])
+                    val latLng = exif.latLong?.let { array ->
+                        LatLng(array[0], array[1])
                     }
                     val picture = Picture(uri, latLng)
 
@@ -37,8 +45,15 @@ class PictureFragment : BaseFragment<FragmentPictureBinding>(R.layout.fragment_p
             }
         }
 
+    private fun navigateToLocationFragment() {
+        val action = PictureFragmentDirections.actionPictureFragmentToLocationFragment()
+        findNavController().navigate(action)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        launchPermissionRequest()
 
         with(binding.toolbarPicture) {
             setNavigationIcon(R.drawable.ic_back)
@@ -53,8 +68,12 @@ class PictureFragment : BaseFragment<FragmentPictureBinding>(R.layout.fragment_p
         }
     }
 
-    private fun navigateToLocationFragment() {
-        val action = PictureFragmentDirections.actionPictureFragmentToLocationFragment()
-        findNavController().navigate(action)
+    private fun launchPermissionRequest() {
+        requestPermissionsLauncher.launch(
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_MEDIA_LOCATION
+            )
+        )
     }
 }
