eca3930fb2aab4e1832381799564cd669e694bd8
신민정 <ydaispaxero@naver.com>
2022-08-23 00:18:49 +0900

    #44 feat: 검색화면에 대한 결과화면 생성

---
+++ presentation/src/main/java/com/cheocharm/presentation/ui/search/SearchResultFragment.kt
@@ -0,0 +1,20 @@
+package com.cheocharm.presentation.ui.search
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.navGraphViewModels
+import com.cheocharm.base.BaseFragment
+import com.cheocharm.presentation.R
+import com.cheocharm.presentation.databinding.FragmentSearchResultBinding
+
+class SearchResultFragment :
+    BaseFragment<FragmentSearchResultBinding>(R.layout.fragment_search_result) {
+
+    private val searchViewModel: SearchViewModel by navGraphViewModels(R.id.search)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewmodel = searchViewModel
+    }
+}
