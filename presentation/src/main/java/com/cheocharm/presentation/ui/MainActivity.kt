c0d063a07ac1ea743e46547943c89140f2baadde
신민정 <ydaispaxero@naver.com>
2022-08-23 00:01:38 +0900

    #44 feat: MainActivity에서 EditText 영역 바깥 부분을 터치하면 키보드 숨김

--- presentation/src/main/java/com/cheocharm/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/cheocharm/presentation/ui/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.cheocharm.presentation.ui
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.cheocharm.base.BaseActivity
@@ -20,6 +25,26 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             val navController = host.navController
 
             binding.bottomNavMain.setupWithNavController(navController)
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        hideKeyboardWhenOutsideTouched(ev)
+        return super.dispatchTouchEvent(ev)
+    }
+
+    private fun hideKeyboardWhenOutsideTouched(ev: MotionEvent?) {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val rect = Rect()
+                v.getGlobalVisibleRect(rect)
+                if (!rect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
         }
     }
 }
