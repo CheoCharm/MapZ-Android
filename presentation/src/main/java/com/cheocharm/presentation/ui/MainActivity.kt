8740414bde3a5d3e823a27f21b621723275364bd
Hyesung82 <khs990419@gmail.com>
2022-08-23 20:56:14 +0900

    fix: 권한 변경 시에도 bottom nav를 초기화하도록 수정

--- presentation/src/main/java/com/cheocharm/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/cheocharm/presentation/ui/MainActivity.kt
@@ -15,15 +15,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (savedInstanceState == null) {
-            val host: NavHostFragment = supportFragmentManager
-                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
-            val navController = host.navController
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navController = host.navController
 
-            binding.bottomNavMain.setupWithNavController(navController)
+        with(binding) {
+            bottomNavMain.setupWithNavController(navController)
+            fragmentMainMap.isVisible = false
         }
-
-        binding.fragmentMainMap.isVisible = false
     }
 
     fun getBinding(): ActivityMainBinding = binding
